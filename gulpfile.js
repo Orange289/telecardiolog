35c1085c6431304c44a010301f948c40a85c7c92
Ann <apelsann@ya.ru>
2016-10-22 19:21:49 +0200

    2210

--- gulpfile.js
+++ gulpfile.js
@@ -6,27 +6,6 @@ var plumber = require("gulp-plumber");
 var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
 var server = require("browser-sync").create();
-var minify = require('gulp-csso');
-var rename = require('gulp-rename');
-var imagemin = require('gulp-imagemin');
-var run = require('run-sequence');
-var del = require('del');
-
-gulp.task('clean', function() {
-	return del('build');
-});
-
-gulp.task('copy', function() {
-	return gulp.src([
-		'fonts/**/*.{woff,woff2}',
-		'img/**',
-		'js/**',
-		'*.html'
-	], {
-		base: '.'
-	})
-	.pipe(gulp.dest('build'));
-});
 
 gulp.task("style", function() {
 	gulp.src("sass/style.scss")
@@ -41,40 +20,18 @@ gulp.task("style", function() {
 				"last 2 Edge versions"
 			]})
 		]))
-		.pipe(gulp.dest("build/css"))
-		.pipe(minify())
-		.pipe(rename('style.min.css'))
-		.pipe(gulp.dest('build/css'))
+		.pipe(gulp.dest("css"))
 		.pipe(server.reload({stream: true}));
 });
 
-gulp.task('images', function() {
-	return gulp.src('build/img/**/*.{png,jpg,gif}')
-		.pipe(imagemin([
-			imagemin.optipng({optimizationLevel: 3}),
-			imagemin.jpegtran({progressive:true})
-		]))
-		.pipe(gulp.dest('build/img'));
-});
-
-gulp.task("serve", function() {
+gulp.task("serve", ["style"], function() {
 	server.init({
-		server: "build"
-		// notify: false,
-		// open: true,
-		// ui: false
+		server: ".",
+		notify: false,
+		open: true,
+		ui: false
 	});
 
 	gulp.watch("sass/**/*.{scss,sass}", ["style"]);
 	gulp.watch("*.html").on("change", server.reload);
-});
-
-gulp.task('build', function(fn) {
-	run(
-		'clean',
-		'copy',
-		'style',
-		'images',
-		fn
-	);
 });
